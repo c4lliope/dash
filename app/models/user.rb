867106a4d244561ad0bdd014271cdb646732cf3f
Grayson Wright <wright.grayson@gmail.com>
2014-12-12 11:15:38 -0800

    Add users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true
-  validates :basecamp_name, presence: true
-  validates :thoughtbot_email_name, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :basecamp_name, presence: true, uniqueness: true
+  validates :thoughtbot_email_name, presence: true, uniqueness: true
+
+  def save
+    self.name ||= basecamp_name.scan(/\w+/).first
+    self.thoughtbot_email_name ||= self.name.downcase
+    super
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+sf_thoughtbotters = [
+  { basecamp_name:"Bernerd Schaefer", thoughtbot_email_name:"b" },
+  { basecamp_name:"Adarsh Pandit" },
+  { basecamp_name:"Carolann Bonner" },
+  { basecamp_name:"Dan Croak" },
+  { basecamp_name:"Galen Frechette" },
+  { basecamp_name:"Carlo Iyog" },
+  { basecamp_name:"Devon St.Clair" },
+  { basecamp_name:"Don Okuda" },
+  { basecamp_name:"Grayson Wright" },
+  { basecamp_name:"Greg Lazarev" },
+  { basecamp_name:"Jeff Smith" },
+  { basecamp_name:"Jessie Young" },
+  { basecamp_name:"Keith Smiley" },
+  { basecamp_name:"Laila Winner" },
+  { basecamp_name:"Mark Adams" },
+  { basecamp_name:"Michelle Venetucci Harvey" },
+  { basecamp_name:"Rich Rines" },
+  { basecamp_name:"Simon Taranto" },
+  { basecamp_name:"Steven Harley" },
+]
+
+sf_thoughtbotters.each do |attrs|
+  User.create(attrs)
+end
