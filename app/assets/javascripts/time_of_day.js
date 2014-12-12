fd351a79cfc05bfe35f178c3031ebe074f374c2e
Steven Harley <smharley@gmail.com>
2014-12-12 11:42:27 -0800

    Adds dynamic date to header

--- app/assets/javascripts/time_of_day.js
+++ app/assets/javascripts/time_of_day.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  function getStylesheet() {
+  function getToD() {
     var currentTime = new Date().getHours();
 
     if (0 <= currentTime&&currentTime < 13) {
@@ -12,5 +12,18 @@ $(document).ready(function() {
       $(".barts").show();
     }
   }
-  getStylesheet();
+  getToD();
+
+  function getDateHeader() {
+    var now = new Date();
+    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+    var day = days[ now.getDay() ];
+    var month = months[ now.getMonth() ];
+    var date = now.getDate();
+
+    $(".current_date").text(day + " | " + month + " " + date);
+  }
+  getDateHeader();
 });
